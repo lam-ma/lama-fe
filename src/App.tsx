8296903418bc0634d940256e4ba4f6e5e9105cab
Mykhailo Kulak <mykhailo.kulak@olx.com>
2020-12-10 20:45:53 +0100

    change secret

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Sucks, bugs, plug'n'play
         </a>
       </header>
     </div>
