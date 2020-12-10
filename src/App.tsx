37fffa4acc0e6d40697a8299537722c73bedb0de
Mykhailo Kulak <mykhailo.kulak@olx.com>
2020-12-10 21:03:00 +0100

    change secret #2

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://lama-api.kvarto.net"
           target="_blank"
           rel="noopener noreferrer"
         >
