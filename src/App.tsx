100448d3b512932fabfbf30e792ac76575b35c19
Mykhailo Kulak <mykhailo.kulak@olx.com>
2020-12-10 20:18:06 +0100

    test changes

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Lama FE FTW
         </p>
         <a
           className="App-link"
