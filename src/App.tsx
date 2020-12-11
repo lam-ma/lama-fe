9ab3c8ea8978bef7befbeb8bf90f0d1a3c2721dc
Leslye Lu Kan <leslye.lukan@olx.com>
2020-12-11 12:14:30 +0100

    add button

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import 'semantic-ui-css/semantic.min.css';
+import StartButton from './Pages/StartQuiz/index';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Lama FE FTW
+          Quizzing time!
         </p>
-        <a
-          className="App-link"
-          href="https://lama-api.kvarto.net"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Stacks, bugs, plug'n'play
-        </a>
-      </header>
+        <StartButton />
     </div>
   );
 }
---
+++ src/Pages/StartQuiz/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Button } from 'semantic-ui-react'
+
+const Start = () => <Button>Start Game</Button>
+
+export default Start
